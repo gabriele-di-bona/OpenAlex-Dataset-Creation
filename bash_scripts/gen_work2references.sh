99b7258f24467a7d6c9cfc617827dcdc5751fc1d
gabriele-di-bona <gabriele.dibona96@gmail.com>
2025-06-25 16:42:43 +0100

    Initial commit: add dataset creation notebooks and scripts

---
+++ bash_scripts/gen_work2references.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#$ -cwd
+#$ -t 66-4516
+#$ -j y
+#$ -pe smp 1 
+#$ -l h_vmem=40G
+# #$ -l highmem
+#$ -l h_rt=1:0:0
+#$ -l rocky
+# #$ -m bae
+# #$ -o /data/scratch/ahw701/FluctuatingDiscoveries/outputs/output
+
+# module load anaconda3
+# conda activate gt
+module load miniforge
+conda activate gt_rocky
+export OMP_NUM_THREADS=1
+# this bash script is supposed to be run from the subfolder outputs, so that the output goes directly there
+cd ../../python_scripts/
+
+python 12_gen_work2references.py -ID ${SGE_TASK_ID}
+
---
+++ python_scripts/.ipynb_checkpoints/12_gen_work2references-checkpoint.py
@@ -0,0 +1,79 @@
+import os
+os.chdir("../") # resets notebook directory to repository root folder (DO ONLY ONCE!)
+import polars as pl
+import pandas as pd
+import pyarrow.parquet as pypq
+import textwrap
+from pathlib import Path
+import time
+
+from tqdm import tqdm
+
+import sys
+# Add utils directory in the list of directories to look for packages to import
+sys.path.insert(0, os.path.join(os.getcwd(),'utils'))
+from read_parquet import *
+
+works_by_topic_parquet_folder = "data/works_by_topic_parquet/"
+works2references_by_topic_parquet_folder = "data/works2references_by_topic_parquet/"
+os.makedirs(works2references_by_topic_parquet_folder, exist_ok=True)
+
+topics = [topic[:-8] for topic in os.listdir(works_by_topic_parquet_folder)]
+
+import argparse
+parser = argparse.ArgumentParser(description='Run this in parallel to speed up.')
+
+parser.add_argument("-ID", "--ID", type=int,
+    help="The index of the topic in topics list (will be subtracted 1, start by 1) [default 1]",
+    default=1)
+
+arguments = parser.parse_args()
+ID = arguments.ID - 1 # unfortunately I have to start this argument from 1 in shell. The -1 is added to start from 0
+topic = topics[ID]
+
+print("reading", flush=True)
+
+all_works2primary_topic_df = read_parquet("data/all_works2primary_topic_parquet/all_works2primary_topic.parquet")
+
+print("read", flush=True)
+
+all_works2primary_topic_df.rename(
+    columns={"id": "referenced_work_id", "date": "referenced_publication_date", "primary_topic": "referenced_primary_topic"},
+    inplace = True
+)
+print("renamed", flush=True)
+
+def generate_reference_expansion(origin_works_parquet_file_path, all_works2primary_topic_df):
+    works_df = read_parquet(origin_works_parquet_file_path, columns = ["id", "date", "references", "primary_topic"], quiet = True)
+    
+    # Step 1: Explode references are exploded into rows
+    works_df = works_df[works_df["references"].apply(lambda x : len(x) > 0)]
+    works_df["references"] = works_df["references"].apply(lambda x : x.split(";"))
+    works_df = works_df.explode("references").rename(columns={"id": "work_id", "date": "publication_date", "references": "referenced_work_id"})
+
+    # Step 2: Join with metadata of the referenced works
+    works_df = works_df.merge(all_works2primary_topic_df, on="referenced_work_id", how="inner")
+
+    # Step 3: Select and reorder the final columns
+    works_df = works_df[[
+        "work_id", "publication_date", "primary_topic",
+        "referenced_work_id", "referenced_publication_date", "referenced_primary_topic"
+    ]]
+
+    return works_df
+    
+def create_works2references_df(origin_works_parquet_file_path, destination_topic_parquet_file_path, all_works2primary_topic_df, compression='brotli', do_peek = True, do_print = True):
+    works2references_df = generate_reference_expansion(origin_works_parquet_file_path, all_works2primary_topic_df)
+    works2references_df.to_parquet(destination_topic_parquet_file_path, compression=compression)
+    if do_print:
+        print(f"Successfully generated {destination_topic_parquet_file_path} using {origin_works_parquet_file_path}.", flush=True)
+    if do_peek:
+        if do_print:
+            print("Here's a peek.", flush=True)
+        peek_parquet(destination_topic_parquet_file_path)
+
+# MAIN CODE
+
+origin_works_parquet_file_path = works_by_topic_parquet_folder+topic+".parquet"
+destination_topic_parquet_file_path = works2references_by_topic_parquet_folder+topic+".parquet"
+create_works2references_df(origin_works_parquet_file_path, destination_topic_parquet_file_path, all_works2primary_topic_df, do_peek = True, do_print = True)
